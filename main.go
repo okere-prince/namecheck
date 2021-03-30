550aea3107281234ff0e701cec897034bb1ba4bc
jubobs <jubobs-oss@protonmail.com>
2021-03-30 17:39:46 +0200

    Move specific stuff to Twitter

--- main.go
+++ main.go
@@ -2,9 +2,8 @@ package main
 
 import (
 	"fmt"
-	"regexp"
-	"strings"
-	"unicode/utf8"
+
+	"github.com/jub0bs/namecheck/twitter"
 )
 
 type Status int
@@ -16,38 +15,7 @@ const (
 	Available
 )
 
-const (
-	minLen         = 1
-	maxLen         = 15
-	illegalPattern = "twitter"
-)
-
-var legalPattern = regexp.MustCompile("^[0-9A-Z_a-z]*$")
-
 func main() {
 	username := "jub0bs"
-	fmt.Println(IsValid(username))
-}
-
-func IsValid(username string) bool {
-	return isLongEnough(username) &&
-		isShortEnough(username) &&
-		containsNoIllegalPattern(username) &&
-		containsOnlyLegalChars(username)
-}
-
-func isLongEnough(username string) bool {
-	return utf8.RuneCountInString(username) >= minLen
-}
-
-func isShortEnough(username string) bool {
-	return utf8.RuneCountInString(username) <= maxLen
-}
-
-func containsNoIllegalPattern(username string) bool {
-	return !strings.Contains(username, strings.ToLower(illegalPattern))
-}
-
-func containsOnlyLegalChars(username string) bool {
-	return legalPattern.MatchString(username)
+	fmt.Println(twitter.IsValid(username))
 }
---
+++ twitter/twitter.go
@@ -0,0 +1,38 @@
+package twitter
+
+import (
+	"regexp"
+	"strings"
+	"unicode/utf8"
+)
+
+const (
+	minLen         = 1
+	maxLen         = 15
+	illegalPattern = "twitter"
+)
+
+var legalPattern = regexp.MustCompile("^[0-9A-Z_a-z]*$")
+
+func IsValid(username string) bool {
+	return isLongEnough(username) &&
+		isShortEnough(username) &&
+		containsNoIllegalPattern(username) &&
+		containsOnlyLegalChars(username)
+}
+
+func isLongEnough(username string) bool {
+	return utf8.RuneCountInString(username) >= minLen
+}
+
+func isShortEnough(username string) bool {
+	return utf8.RuneCountInString(username) <= maxLen
+}
+
+func containsNoIllegalPattern(username string) bool {
+	return !strings.Contains(username, strings.ToLower(illegalPattern))
+}
+
+func containsOnlyLegalChars(username string) bool {
+	return legalPattern.MatchString(username)
+}
